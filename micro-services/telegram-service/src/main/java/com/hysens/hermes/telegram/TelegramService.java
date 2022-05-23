9b51a829eb0c477c4f9ec8d4bc85941210957471
Vladislavs Gordejevs <vladislavs.gordejevs@gmail.com>
2022-05-24 01:37:13 +0300

    Added @Service annotations for Telegram & WhatsApp services for dependency injection.

--- micro-services/telegram-service/src/main/java/com/hysens/hermes/telegram/TelegramService.java
+++ micro-services/telegram-service/src/main/java/com/hysens/hermes/telegram/TelegramService.java
@@ -1,8 +1,11 @@
 package com.hysens.hermes.telegram;
 
 import com.hysens.hermes.common.service.MessageService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TelegramService implements MessageService {
+
     @Override
     public boolean sendMessage(String phoneNumber, String message) {
         return false;
--- micro-services/whatsapp-service/src/main/java/com/hysens/hermes/whatsapp/WhatsAppService.java
+++ micro-services/whatsapp-service/src/main/java/com/hysens/hermes/whatsapp/WhatsAppService.java
@@ -1,10 +1,14 @@
 package com.hysens.hermes.whatsapp;
 
 import com.hysens.hermes.common.service.MessageService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WhatsAppService implements MessageService {
+
     @Override
     public boolean sendMessage(String phoneNumber, String message) {
         return false;
     }
+
 }
