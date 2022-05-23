d660d4313fdc70acb2975943dcac7149a25b3e7f
Vladislavs Gordejevs <vladislavs.gordejevs@gmail.com>
2022-05-23 23:17:40 +0300

    Added initial security configuration.

---
+++ src/main/java/com/hysens/hermes/configuration/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.hysens.hermes.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .antMatchers("/api/v1/user/login", "/api/v1/user/register")
+                            .permitAll()
+                        .antMatchers("/**")
+                            .authenticated()
+                )
+                .csrf().disable()
+                .cors().and()
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
